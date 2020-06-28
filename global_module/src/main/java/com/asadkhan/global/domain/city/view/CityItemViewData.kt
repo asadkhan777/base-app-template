bb409deeb2f032cdd1a425996cc19cf3b4ae0484
Asad Khan <asad.khan@shadowfax.in>
2020-06-28 20:20:46 +0530

    Add short form of City Details format for interim viewing

--- global_module/src/main/java/com/asadkhan/global/domain/city/view/CityItemViewData.kt
+++ global_module/src/main/java/com/asadkhan/global/domain/city/view/CityItemViewData.kt
@@ -1,2 +1,15 @@
 package com.asadkhan.global.domain.city.view
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class CityItemViewData(
+    val fullName: String,
+    val cityLinkUrl: String
+) : Parcelable {
+  fun getGeoCodeID() = cityLinkUrl.split(":")[2].replace("/", "").toInt()
+  fun getCityName() = fullName.split(",")[0].trim()
+  fun getUrbanAreaName() = fullName.split(",")[1].trim()
+  fun getCountryName() = fullName.split(",")[2].trim()
+}
