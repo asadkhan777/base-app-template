1c24ebfeb69a4f60d0e103d7cb6f1ded227c833a
Asad Khan <asad.khan@shadowfax.in>
2020-06-29 01:25:01 +0530

    Update tests to use androidx

--- global_module/src/androidTest/java/com/asadkhan/global/ExampleInstrumentedTest.java
+++ global_module/src/androidTest/java/com/asadkhan/global/ExampleInstrumentedTest.java
@@ -2,8 +2,10 @@ package com.asadkhan.global;
 
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
