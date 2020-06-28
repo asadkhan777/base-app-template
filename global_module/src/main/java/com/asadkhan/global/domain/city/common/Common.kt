3434fb3699bbad50f4aa61788b1b2220acd18402
Asad Khan <asad.khan@shadowfax.in>
2020-06-28 20:23:31 +0530

    Add Domain entity classes common to all flows

--- global_module/src/main/java/com/asadkhan/global/domain/city/common/Common.kt
+++ global_module/src/main/java/com/asadkhan/global/domain/city/common/Common.kt
@@ -1,2 +1,39 @@
 package com.asadkhan.global.domain.city.common
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class TeleportLink(
+    @SerializedName("href")
+    val href: String
+) : Parcelable
+
+
+@Parcelize
+data class TeleportLinkEntity(
+    @SerializedName("href")
+    val href: String,
+    @SerializedName("name")
+    val name: String
+) : Parcelable
+
+
+@Parcelize
+data class Coordinates(
+    @SerializedName("geohash")
+    val geoHash: String,
+    @SerializedName("latlon")
+    val latLong: LatLong
+) : Parcelable
+
+
+@Parcelize
+data class LatLong(
+    @SerializedName("latitude")
+    val latitude: Double, // 33.729388
+    @SerializedName("longitude")
+    val longitude: Double // 73.093146
+) : Parcelable
