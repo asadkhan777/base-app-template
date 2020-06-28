6fa41581078064599ceff3bf14f34645d7ad9cd2
Asad Khan <asad.khan@shadowfax.in>
2020-06-29 00:28:43 +0530

    Add domain specific Utils class

--- global_module/src/main/java/com/asadkhan/global/DomainUtils.kt
+++ global_module/src/main/java/com/asadkhan/global/DomainUtils.kt
@@ -1,2 +1,22 @@
 package com.asadkhan.global
 
+import com.asadkhan.global.database.city.CityDetails
+import com.asadkhan.global.domain.city.details.CityDetailsData
+
+fun CityDetailsData.convertToEntity(favInput: Boolean = isFavorite): CityDetails {
+  val details = cityDetailLinks
+  return CityDetails(
+      fullName = fullName,
+      geonameId = geonameId,
+      latitude = location.latLong.latitude,
+      longitude = location.latLong.longitude,
+      name = name,
+      population = population,
+      province = details.cityAdmin1Division.name,
+      country = details.cityCountry.name,
+      timezone = details.cityTimezone.name,
+      urbanArea = details.cityUrbanArea?.name ?: details.cityAdmin1Division.name,
+      selfUrl = details.selfCityLink.href,
+      isFavorite = favInput
+  )
+}
