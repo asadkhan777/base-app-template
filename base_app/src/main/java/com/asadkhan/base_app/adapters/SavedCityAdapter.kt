28febdd86fc0d768d62beacc7088d65b2aca2a5c
Asad Khan <asad.khan@shadowfax.in>
2020-06-29 00:36:39 +0530

    Add View layer classes for Saved Cities feature

--- base_app/src/main/java/com/asadkhan/base_app/adapters/SavedCityAdapter.kt
+++ base_app/src/main/java/com/asadkhan/base_app/adapters/SavedCityAdapter.kt
@@ -1,17 +1,58 @@
 package com.asadkhan.base_app.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.asadkhan.base_app.CityDetailsPage
+import com.asadkhan.base_app.Navigator
+import com.asadkhan.base_app.R
+import com.asadkhan.global.base.BaseViewHolder
+import com.asadkhan.global.database.city.CityDetails
+import kotlinx.android.synthetic.main.view_holder_saved_city.view.tv_saved_city_name
 
-internal class SavedCityAdapter : Adapter<SavedCityViewHolder>() {
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedCityViewHolder {
-    return null
+class SavedCityAdapter(private val context: Context) : Adapter<BaseViewHolder>() {
+  
+  private val savedCitiesList = ArrayList<CityDetails>()
+  
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+    val view = LayoutInflater.from(context).inflate(R.layout.view_holder_saved_city, parent, false)
+    return SavedCityViewHolder(view)
   }
   
-  override fun onBindViewHolder(holder: SavedCityViewHolder, position: Int) {}
-  override fun getItemCount(): Int {
-    return 0
+  override fun getItemCount() = savedCitiesList.size
+  
+  override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+    if (holder is SavedCityViewHolder) {
+      val adapterPosition = holder.adapterPosition
+      val cityDetails = savedCitiesList[adapterPosition]
+      with(holder.itemView) {
+        this.setOnClickListener {
+          Navigator.navigationLiveData.postValue(CityDetailsPage(cityDetails.geonameId))
+        }
+        tv_saved_city_name.text = cityDetails.name
+      }
+    }
   }
+  
+  fun updateItems(citiesList: List<CityDetails>?) {
+    if (citiesList != null && citiesList.isNotEmpty()) {
+      savedCitiesList.clear()
+      savedCitiesList.addAll(citiesList)
+      notifyDataSetChanged()
+    }
+  }
+  
+  fun clear() {
+    savedCitiesList.clear()
+    notifyDataSetChanged()
+  }
+  
 }
 
-internal class SavedCityViewHolder 
+class SavedCityViewHolder(itemView: View) : BaseViewHolder(itemView) {
+
+
+}
+
--- base_app/src/main/java/com/asadkhan/base_app/screens/MySavedCitiesFragment.kt
+++ base_app/src/main/java/com/asadkhan/base_app/screens/MySavedCitiesFragment.kt
@@ -1,5 +1,55 @@
 package com.asadkhan.base_app.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.asadkhan.base_app.R
+import com.asadkhan.base_app.adapters.SavedCityAdapter
+import com.asadkhan.base_app.viewmodels.CityViewModel
 import com.asadkhan.global.base.BaseFragment
+import com.asadkhan.global.hide
+import com.asadkhan.global.show
+import kotlinx.android.synthetic.main.fragment_saved_cities.iv_empty_state
+import kotlinx.android.synthetic.main.fragment_saved_cities.rv_saved_cities
+import kotlinx.android.synthetic.main.fragment_saved_cities.tv_empty_state
 
-class MySavedCitiesFragment : BaseFragment() 
+class MySavedCitiesFragment : BaseFragment() {
+  
+  private val cityViewModel by lazy { viewModelProvider.get(CityViewModel::class.java) }
+  private val savedCityAdapter by lazy { SavedCityAdapter(requireActivity()) }
+  
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_saved_cities, container, false)
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+  
+    rv_saved_cities.apply {
+      layoutManager = LinearLayoutManager(requireActivity())
+      adapter = savedCityAdapter
+    }
+    
+    cityViewModel.savedCitiesListLiveData.observe(viewLifecycleOwner, Observer { citiesList ->
+      if (citiesList == null || citiesList.isEmpty()) {
+        savedCityAdapter.clear()
+        
+        rv_saved_cities.hide()
+        iv_empty_state.show()
+        tv_empty_state.show()
+      } else {
+        savedCityAdapter.updateItems(citiesList)
+        
+        rv_saved_cities.show()
+        iv_empty_state.hide()
+        tv_empty_state.hide()
+      }
+    })
+    
+    cityViewModel.fetchSavedCities()
+  }
+  
+}
