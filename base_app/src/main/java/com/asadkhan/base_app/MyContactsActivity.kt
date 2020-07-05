bbdc6b309c53641152d122aa719930e09622f627
Asad Khan <asad.khan@shadowfax.in>
2020-07-05 20:30:04 +0530

    Add UI & Layout for Main Contacts Activity

--- base_app/src/main/java/com/asadkhan/base_app/MyContactsActivity.kt
+++ base_app/src/main/java/com/asadkhan/base_app/MyContactsActivity.kt
@@ -1,16 +1,16 @@
 package com.asadkhan.base_app
 
-import android.app.Activity
 import android.os.Bundle
 import android.os.Handler
 import com.asadkhan.global.Constants
-import kotlinx.android.synthetic.main.activity_main.tv_hello_world
+import com.asadkhan.global.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_my_contacts.tv_hello_world
 
-class MainActivity : Activity() {
+class MyContactsActivity : BaseActivity() {
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_my_contacts)
     Handler().postDelayed({
       tv_hello_world.text = getString(R.string.hello_world)
     }, 2000)
